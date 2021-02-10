02989afbb81a1bb2b9916c5b595890f5143b440e
Tyler <tyler@anymod.com>
2021-02-10 10:18:55 -0800

    Update to add signup form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Userfront from "@userfront/react";
+Userfront.init("demo1234");
+
+const SignupForm = Userfront.build({
+  toolId: "nkmbbm",
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <SignupForm />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
